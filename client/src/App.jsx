461d948491e0ba5da9691ac01878b589af09f41d
MinhHoang137 <nhakhoahoc102@gmail.com>
2025-11-12 22:42:11 +0700

    cart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,10 @@ import ProductDetail from './Pages/ProductDetail/ProductDetail.jsx'
 import Login from "./Pages/Login/Login.jsx";
 import SignUp from "./Pages/SignUp/SignUp.jsx";
 import AboutUs from "./Pages/AboutUs/AboutUs.jsx";
+import Cart from "./Pages/Cart";
+import MyAccount from "./Pages/MyAccount";
+import EditAddress from "./Pages/Addresses/Edit";
+
 
 function App() {
   return (
@@ -17,6 +21,10 @@ function App() {
             <Route path="/login" exact={true} element={<Login/>}/>
             <Route path="/signup" exact={true} element={<SignUp/>}/>
             <Route path="/about_us" exact={true} element={<AboutUs/>}/>
+            <Route path="/cart" exact={true} element={<Cart/>}/>
+            <Route path="/my_account" exact={true} element={<MyAccount/>}/>
+            <Route path="/addresses/new" exact={true} element={<EditAddress/>}/>
+            <Route path="/addresses/edit/:id" exact={true} element={<EditAddress/>}/>
           </Routes>
       </BrowserRouter>
       
--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -3,6 +3,7 @@ import './login.css';
 
 const Login = () => {
   return (
+    
     <div className="login-container">
       <div className="login-box">
         <div className="input-group">
